03ab73307fce01e21100d3be99b14b688dfd0b5a
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2021-03-16 23:28:38 +0100

    feat: Added constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+export default {
+  white: '#FFFFFF',
+  dark: '#34495e',
+  grey: '#95a5a6',
+  light: '#ecf0f1',
+};
---
+++ src/constants/fonts.ts
@@ -0,0 +1,45 @@
+import {hp} from '../shared/layout';
+import colors from './colors';
+
+export const fonts = {
+  50: {
+    fontSize: hp(50),
+    lineHeight: hp(60),
+    color: colors.dark,
+  },
+  30: {
+    fontSize: hp(30),
+    lineHeight: hp(40),
+    color: colors.dark,
+  },
+  24: {
+    fontSize: hp(24),
+    lineHeight: hp(36),
+    color: colors.dark,
+  },
+  20: {
+    fontSize: hp(20),
+    lineHeight: hp(30),
+    color: colors.dark,
+  },
+  18: {
+    fontSize: hp(18),
+    lineHeight: hp(27),
+    color: colors.dark,
+  },
+  16: {
+    fontSize: hp(16),
+    lineHeight: hp(24),
+    color: colors.dark,
+  },
+  14: {
+    fontSize: hp(14),
+    lineHeight: hp(21),
+    color: colors.dark,
+  },
+  12: {
+    fontSize: hp(12),
+    lineHeight: hp(18),
+    color: colors.dark,
+  },
+};
